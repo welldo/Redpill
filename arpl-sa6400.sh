d0bbc86be50cda6ef8f2e9c6a41d04abb62f64e5
Ing <wjz304@qq.com>
2023-02-06 19:53:41 +0800

    ARPL SA6400

--- arpl-sa6400.sh
+++ arpl-sa6400.sh
@@ -1,18 +1,35 @@
 #!/usr/bin/env bash
 
 ROOT_PATH=$PWD
+TEMP_PATH=/tmp
+RDXZ_PATH=${TEMP_PATH}/rdxz_tmp
 
 echo Opening..
-
-RDXZ_PATH=${ROOT_PATH}/rdxz_tmp
-mkdir -p "${RDXZ_PATH}"; (cd "${RDXZ_PATH}"; xz -dc < "/mnt/p3/initrd-arpl" | cpio -idm) >/dev/null 2>&1 || true
-if [ -f "${RDXZ_PATH}/opt/arpl/model-configs/SA6400.yml.disabled" ]; then
-  cd "${RDXZ_PATH}"
-  mv "opt/arpl/model-configs/SA6400.yml.disabled" "opt/arpl/model-configs/SA6400.yml"
-  find . 2>/dev/null | cpio -o -H newc -R root:root | xz --check=crc32 > "/mnt/p3/initrd-arpl"
-  cd "${ROOT_PATH}"
-  mv "/opt/arpl/model-configs/SA6400.yml.disabled" "/opt/arpl/model-configs/SA6400.yml"
+if [ -f "/opt/arpl/model-configs/SA6400.yml" ]; then
+  echo "SA6400 is exist!"
+  exit 0
 fi
+
+sed -i 's/global.download.synology.com/cndl.synology.cn/g' /opt/arpl/menu.sh
+sed -i 's/global.download.synology.com/cndl.synology.cn/g' `find /opt/arpl/model-configs/ -type f`
+[ -z "$(grep "inetd" /opt/arpl/ramdisk-patch.sh)" ] && sed -i '/# Build modules dependencies/i\# Enable Telnet\necho "inetd" >> "${RAMDISK_PATH}/addons/addons.sh"\n' /opt/arpl/ramdisk-patch.sh
+
+if [ -f "/opt/arpl/model-configs/SA6400.yml.disabled" ]; then
+  mv "/opt/arpl/model-configs/SA6400.yml.disabled" "/opt/arpl/model-configs/SA6400.yml"
+else
+  curl -skL "https://github.com/wjz304/Redpill_CustomBuild/files/10660712/SA6400.yml.gz" -o "/opt/arpl/model-configs/SA6400.yml.gz"
+  gzip -d "/opt/arpl/model-configs/SA6400.yml.gz"
+fi
+
+if [ ! -f "/opt/arpl/patch/ramdisk-common-init-script-v5.patch" ]; then
+  curl -skL "https://github.com/wjz304/Redpill_CustomBuild/files/10660181/ramdisk-common-init-script-v5.patch.gz" -o "/opt/arpl/patch/ramdisk-common-init-script-v5.patch.gz"
+  gzip -d "/opt/arpl/patch/ramdisk-common-init-script-v5.patch.gz"
+fi
+
+mkdir -p "${RDXZ_PATH}"
+(cd "${RDXZ_PATH}"; xz -dc < "/mnt/p3/initrd-arpl" | cpio -idm) >/dev/null 2>&1 || true
+cp -rf "/opt/arpl" "${RDXZ_PATH}/opt"
+(cd "${RDXZ_PATH}"; find . 2>/dev/null | cpio -o -H newc -R root:root | xz --check=crc32 > "/mnt/p3/initrd-arpl") || true
 rm -rf "${RDXZ_PATH}"
 
 [ ! -f "/mnt/p3/modules/epyc7002-5.10.55.tgz" ] && curl -skL "https://github.com/fbelavenuto/arpl-modules/releases/download/v1.63/epyc7002-5.10.55.tgz" -o "/mnt/p3/modules/epyc7002-5.10.55.tgz"
