2ca1f38b1bb01a8475a8049d70427d16ce209a86
Ing <wjz304@qq.com>
2023-02-15 04:10:14 +0800

    fix something

--- arpl-sa6400.sh
+++ arpl-sa6400.sh
@@ -14,16 +14,15 @@ sed -i 's/global.download.synology.com/cndl.synology.cn/g' /opt/arpl/menu.sh
 sed -i 's/global.download.synology.com/cndl.synology.cn/g' `find /opt/arpl/model-configs/ -type f`
 [ -z "$(grep "inetd" /opt/arpl/ramdisk-patch.sh)" ] && sed -i '/# Build modules dependencies/i\# Enable Telnet\necho "inetd" >> "${RAMDISK_PATH}/addons/addons.sh"\n' /opt/arpl/ramdisk-patch.sh
 
-if [ -f "/opt/arpl/model-configs/SA6400.yml.disabled" ]; then
-  mv "/opt/arpl/model-configs/SA6400.yml.disabled" "/opt/arpl/model-configs/SA6400.yml"
-else
-  curl -skL "https://github.com/wjz304/Redpill_CustomBuild/files/10660712/SA6400.yml.gz" -o "/opt/arpl/model-configs/SA6400.yml.gz"
-  gzip -d "/opt/arpl/model-configs/SA6400.yml.gz"
-fi
+[ -f "/opt/arpl/model-configs/SA6400.yml.disabled" ] && rm -f "/opt/arpl/model-configs/SA6400.yml.disabled"
+
+curl -skL "https://github.com/wjz304/Redpill_CustomBuild/files/10736435/SA6400.yml.gz" -o "/opt/arpl/model-configs/SA6400.yml.gz"
+gzip -d "/opt/arpl/model-configs/SA6400.yml.gz"
+
 
 if [ ! -f "/opt/arpl/patch/ramdisk-common-init-script-v5.patch" ]; then
-  curl -skL "https://github.com/wjz304/Redpill_CustomBuild/files/10660181/ramdisk-common-init-script-v5.patch.gz" -o "/opt/arpl/patch/ramdisk-common-init-script-v5.patch.gz"
-  gzip -d "/opt/arpl/patch/ramdisk-common-init-script-v5.patch.gz"
+  curl -skL "https://github.com/wjz304/Redpill_CustomBuild/files/10736439/ramdisk-sa6400-init-script.patch.gz" -o "/opt/arpl/patch/ramdisk-sa6400-init-script.patch.gz"
+  gzip -d "/opt/arpl/patch/ramdisk-sa6400-init-script.patch.gz"
 fi
 
 mkdir -p "${RDXZ_PATH}"
@@ -36,13 +35,7 @@ rm -rf "${RDXZ_PATH}"
 TAG=`curl -skL https://api.github.com/repos/fbelavenuto/arpl-modules/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3)}'`
 [ ! -f "/mnt/p3/modules/epyc7002-5.10.55.tgz" ] && curl -skL "https://github.com/fbelavenuto/arpl-modules/releases/download/${TAG}/epyc7002-5.10.55.tgz" -o "/mnt/p3/modules/epyc7002-5.10.55.tgz"
 #jim
-#[ ! -f "/mnt/p3/lkms/rp-epyc7002-5.10.55-dev.ko.gz" ] && curl -skL "https://github.com/wjz304/Redpill_CustomBuild/files/10607931/rp-epyc7002-5.10.55-dev.ko.gz" -o "/mnt/p3/lkms/rp-epyc7002-5.10.55-dev.ko.gz"
-#[ ! -f "/mnt/p3/lkms/rp-epyc7002-5.10.55-prod.ko.gz" ] && curl -skL "https://github.com/wjz304/Redpill_CustomBuild/files/10607932/rp-epyc7002-5.10.55-prod.ko.gz" -o "/mnt/p3/lkms/rp-epyc7002-5.10.55-prod.ko.gz"
-#poco
-#[ ! -f "/mnt/p3/lkms/rp-epyc7002-5.10.55-dev.ko.gz" ] && curl -skL "https://github.com/wjz304/Redpill_CustomBuild/files/10689263/rp-epyc7002-5.10.55-dev.ko.gz" -o "/mnt/p3/lkms/rp-epyc7002-5.10.55-dev.ko.gz"
-#[ ! -f "/mnt/p3/lkms/rp-epyc7002-5.10.55-prod.ko.gz" ] && curl -skL "https://github.com/wjz304/Redpill_CustomBuild/files/10689264/rp-epyc7002-5.10.55-prod.ko.gz" -o "/mnt/p3/lkms/rp-epyc7002-5.10.55-prod.ko.gz"
-#my
-[ ! -f "/mnt/p3/lkms/rp-epyc7002-5.10.55-dev.ko.gz" ] && curl -skL "https://github.com/wjz304/Redpill_CustomBuild/files/10714519/rp-epyc7002-5.10.55-dev.ko.gz" -o "/mnt/p3/lkms/rp-epyc7002-5.10.55-dev.ko.gz"
-[ ! -f "/mnt/p3/lkms/rp-epyc7002-5.10.55-prod.ko.gz" ] && curl -skL "https://github.com/wjz304/Redpill_CustomBuild/files/10714518/rp-epyc7002-5.10.55-prod.ko.gz" -o "/mnt/p3/lkms/rp-epyc7002-5.10.55-prod.ko.gz"
+[ ! -f "/mnt/p3/lkms/rp-epyc7002-5.10.55-dev.ko.gz" ] && curl -skL "https://github.com/wjz304/Redpill_CustomBuild/files/10734771/rp-epyc7002-5.10.55-dev.ko.gz" -o "/mnt/p3/lkms/rp-epyc7002-5.10.55-dev.ko.gz"
+[ ! -f "/mnt/p3/lkms/rp-epyc7002-5.10.55-prod.ko.gz" ] && curl -skL "https://github.com/wjz304/Redpill_CustomBuild/files/10734770/rp-epyc7002-5.10.55-prod.ko.gz" -o "/mnt/p3/lkms/rp-epyc7002-5.10.55-prod.ko.gz"
 
 echo "Open SA6400 OK!"
